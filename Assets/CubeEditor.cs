8a22e70baa756dec167621e4d36fbec06b73b107
lovedakoko <dkoko002@gmail.com>
2020-10-11 15:55:03 -0400

    Added grid-labels to the blocks using TextMesh, and editing the auto-snapping script to provide and update the grid values to the text mesh based on the blocks position

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -10,28 +10,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
     [Header("Grid Parameters")]
     [Range(1f,20f)][SerializeField] float gridSize = 10f;
 
-    void Awake()
-    {
-        Debug.Log("Editor causes this Awake");
-    }
+    TextMesh textMesh;
 
     void Update()
     {
         Vector3 snapPos; //This vector represents the snapped position of the object
+        Vector3 gridSizeFactor;  //This variable is personal choice -> I made it to contain the actual normalized grid value according to the transform of the object
+        //and the gridSize we select
+
         //The following three lines are, respectively for the x, y, and z components of snapPos, the integer-rounded values of the position of the object
         //Each line takes your coordinate-position float-value, divides it by the grid size value, produces a factor which indicates how many grid size values are
         // contained in the transformed position value, uses Mathf.RoundtoInt to round this factor to the nearest whole number (corresponding to the nearest "whole" grid value),      
-        // and converts the rounded factor back to this "whole" grid-value by multiplying the said rounded factor by the grid size
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; 
-        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        // and converts the rounded factor back to this "whole grid-value by multiplying the said rounded factor by the grid size
+
+        gridSizeFactor.x = Mathf.RoundToInt(transform.position.x / gridSize);
+        gridSizeFactor.y = Mathf.RoundToInt(transform.position.y / gridSize);
+        gridSizeFactor.z = Mathf.RoundToInt(transform.position.z / gridSize);
+
+        snapPos.x = gridSizeFactor.x * gridSize; 
+        snapPos.y = gridSizeFactor.y * gridSize;
+        snapPos.z = gridSizeFactor.z * gridSize;
 
         transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = gridSizeFactor.x + "," + gridSizeFactor.z;
     }
 }
 
