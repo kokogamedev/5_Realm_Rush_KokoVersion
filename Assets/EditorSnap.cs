9ba9e5e1311494dc5897ec9d4dafd5f231ee54f9
lovedakoko <dkoko002@gmail.com>
2020-10-11 13:39:07 -0400

    Forgot to add this script to prior commit

--- Assets/EditorSnap.cs
+++ Assets/EditorSnap.cs
@@ -22,8 +22,12 @@ public class EditorSnap : MonoBehaviour
 
     void Update()
     {
-        Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        Vector3 snapPos; //This vector represents the snapped position of the object
+        //The following three lines are, respectively for the x, y, and z components of snapPos, the integer-rounded values of the position of the object
+        //Each line takes your coordinate-position float-value, divides it by the grid size value, produces a factor which indicates how many grid size values are
+        // contained in the transformed position value, uses Mathf.RoundtoInt to round this factor to the nearest whole number (corresponding to the nearest "whole" grid value),      
+        // and converts the rounded factor back to this "whole" grid-value by multiplying the said rounded factor by the grid size
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize; 
         snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
