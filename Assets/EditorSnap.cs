b6617f82ca4a0ce87739cf385ed0aed4f1ba5f45
lovedakoko <dkoko002@gmail.com>
2020-10-11 13:38:07 -0400

    Added an automatic, customizable snapping script for the quad-cube that will allow the cubes to be automatically placed at discrete intervals from within the editor

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,33 @@
+﻿// The PrintAwake script is placed on a GameObject.  The Awake function is
+// called when the GameObject is started at runtime.  The script is also
+// called by the Editor.  An example is when the Scene is changed to a
+// different Scene in the Project window.
+// The Update() function is called, for example, when the GameObject transform
+// position is changed in the Editor.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Header("Grid Parameters")]
+    [Range(1f,20f)][SerializeField] float gridSize = 10f;
+
+    void Awake()
+    {
+        Debug.Log("Editor causes this Awake");
+    }
+
+    void Update()
+    {
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
+    }
+}
+
