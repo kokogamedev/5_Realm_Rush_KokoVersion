c6994e4915cce2d6089e09aee673b1983c8c0a39
lovedakoko <dkoko002@gmail.com>
2020-10-15 12:27:29 -0400

    Vid 131 tasks completed + added forgotten [SelectionBase] code = accomplished so decoupling of scripts, specifically the CubeEditor.cs and Waypoint.cs scripts

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -13,35 +13,39 @@ using UnityEngine;
 [SelectionBase]
 public class CubeEditor : MonoBehaviour
 {
-    [Header("Grid Parameters")]
-    [Range(1f,20f)][SerializeField] float gridSize = 10f;
 
-    TextMesh textMesh;
+    Waypoint waypoint;
+
+    void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
 
     void Update()
     {
-        Vector3 snapPos; //This vector represents the snapped position of the object
-        Vector3 gridSizeFactor;  //This variable is personal choice -> I made it to contain the actual normalized grid value according to the transform of the object
-        //and the gridSize we select
+        SnaptoGrid();
+        UpdateLabel();
+    }
 
-        //The following three lines are, respectively for the x, y, and z components of snapPos, the integer-rounded values of the position of the object
+    private void SnaptoGrid()
+    {
+        //The following  lines are, respectively for the x, y, and z components of snapPos, the integer-rounded values of the position of the object
         //Each line takes your coordinate-position float-value, divides it by the grid size value, produces a factor which indicates how many grid size values are
         // contained in the transformed position value, uses Mathf.RoundtoInt to round this factor to the nearest whole number (corresponding to the nearest "whole" grid value),      
         // and converts the rounded factor back to this "whole grid-value by multiplying the said rounded factor by the grid size
+        int gridSize = waypoint.GetGridSize();
+        Vector2 gridPos = waypoint.GetGridWorldPos();
+        transform.position = new Vector3(gridPos.x, 0, gridPos.y);
+    }
 
-        gridSizeFactor.x = Mathf.RoundToInt(transform.position.x / gridSize);
-        gridSizeFactor.y = Mathf.RoundToInt(transform.position.y / gridSize);
-        gridSizeFactor.z = Mathf.RoundToInt(transform.position.z / gridSize);
-        string labelText = gridSizeFactor.x + "," + gridSizeFactor.z;
+    private void UpdateLabel()
+    {
+        Vector2 gridCoord = waypoint.GetGridPos();
+        string labelText = gridCoord.x + "," + gridCoord.y;
 
-        snapPos.x = gridSizeFactor.x * gridSize; 
-        snapPos.y = gridSizeFactor.y * gridSize;
-        snapPos.z = gridSizeFactor.z * gridSize;
-        transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
-
-        textMesh = GetComponentInChildren<TextMesh>();
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
         textMesh.text = labelText;
-        
+
         gameObject.name = labelText;
     }
 }
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
+    const int gridSize = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +16,38 @@ public class Waypoint : MonoBehaviour
     {
         
     }
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    public Vector3 GetGridPos()
+    {
+        Vector3 gridCoord3D;  //This variable is personal choice -> I made it to contain the actual normalized grid value according to the transform of the object
+                              //and the gridSize we select
+
+        gridCoord3D.x = Mathf.RoundToInt(transform.position.x / gridSize);
+        gridCoord3D.y = Mathf.RoundToInt(transform.position.y / gridSize);
+        gridCoord3D.z = Mathf.RoundToInt(transform.position.z / gridSize);
+
+        return new Vector2(gridCoord3D.x, gridCoord3D.z);
+    }
+
+    public Vector3 GetGridWorldPos()
+    {
+        Vector3 gridPos3D; //This vector represents the snapped position of the object
+        Vector2 gridCoord = GetGridPos();  //This variable is personal choice -> I made it to contain the actual normalized grid value according to the transform of the object
+                            //and the gridSize we select
+
+        gridPos3D.x = gridCoord.x * gridSize;
+        gridPos3D.z = gridCoord.y * gridSize;
+
+        return new Vector2(gridPos3D.x, gridPos3D.z);
+
+    }
+
+
+
 }
+
+
