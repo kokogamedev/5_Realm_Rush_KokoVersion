f7c68bfef47b40960fdd9d0fc5629006165ccb5b
lovedakoko <dkoko002@gmail.com>
2020-10-18 20:18:38 -0400

    Vid 135 Objectives Completed - created a direction array that stored the Vector2 displacement directions for exploring neighboring grid-blocks, and created a method that colored all tops of blocks neighboring the starting block

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -8,14 +8,36 @@ public class Pathfinder : MonoBehaviour
     Dictionary<Vector2, Waypoint> grid = new Dictionary<Vector2, Waypoint>();
     [SerializeField] Waypoint startWaypoint, endWaypoint;
     Waypoint waypoint;
+    Vector2[] directions = { Vector2.up,
+                            Vector2.right,
+                            Vector2.down,
+                            Vector2.left };
+
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbors();
     }
 
+    private void ExploreNeighbors()
+    {
+        foreach (Vector2 direction in directions)
+        {
+            Vector2 explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            //print("Exploring " + explorationCoordinates);
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.white);
+            }
+            catch
+            {
+                //do nothing
+            }
+        }
+    }
 
     private void LoadBlocks()
     {
