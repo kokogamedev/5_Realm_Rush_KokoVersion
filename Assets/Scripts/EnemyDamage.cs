4e9c1851136ed2c3a2e249f5eb3979efc5491614
lovedakoko <dkoko002@gmail.com>
2020-10-25 12:54:53 -0400

    Began commenting - commented all codes written during solo challenge to keep track of changes - this marks the partial movement from note-taking to code-commenting

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
-    AudioSource audioSource;
+    AudioSource audioSource; //initializing the audiosource from which we will play sound effects
 
-    [SerializeField] AudioClip impactSound;
-    [SerializeField] AudioClip deathSound;
-    [SerializeField] GameObject deathFX;
+    [SerializeField] AudioClip impactSound; //initializing the audioclip for weapon impacts on enemy
+    [SerializeField] AudioClip deathSound; //initializing the audioclip for death/explosion sounds for enemy
+    [SerializeField] GameObject deathFX; //initializing the particle system for enemy death (explosion)
 
-    [SerializeField] int hits = 5;
-    int scorePerHit = 10;
-    ScoreBoard scoreBoard;
+    [SerializeField] int hits = 5; //initializing number of hits required for enemy death
+    int scorePerHit = 10; //initializing the points/score you receive for every weapon impact on enemy
+    ScoreBoard scoreBoard; //initializing the Scoreboard script located on the scoreboard - allows us to update/extract from said script
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        scoreBoard = FindObjectOfType<ScoreBoard>();
+        audioSource = GetComponent<AudioSource>(); //At start, extract audiosource component and set it to equal our initialized audioSource variable
+        scoreBoard = FindObjectOfType<ScoreBoard>(); //At start, find an object in our scene of type Scoreboard (find the Scoreboard script and its associated scoreboard game object)
     }
 
     // Update is called once per frame
@@ -27,35 +27,35 @@ public class EnemyDamage : MonoBehaviour
         
     }
 
-    void OnParticleCollision(GameObject other)
+    void OnParticleCollision(GameObject other) //This method processes collisions of a particle system's particles on your game object - this method will handle bullet impacts on your game object
     {
         ProcessHit();
-        if (hits < 1)
+        if (hits < 1) //destroys enemy and initiates explosion particle and sound FX if hitpoints is below 1
         {
             KillEnemy();
         }
-        else
+        else //plays the impactSound SFX (particle effect handled within Unity editor - a subsystem particle explosion effect was added to the main particle system for whenever a collision occurs)
         {
             PlayHitNoise();
         }
     }
 
-    private void ProcessHit()
+    private void ProcessHit() //this method subtracts  from your gameObject's health/hitpoints, and updates the score on the scoreboard
     {
-        hits--;
+        hits--; 
         scoreBoard.ScoreHit(scorePerHit);
 
         //todo: consider hit effects
     }
 
-    private void KillEnemy()
+    private void KillEnemy() //instantiates a self-destroying explosion particle system at location of game object (and no rotation), and destroys your game object
     {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         //fx.transform.parent = parent;
         Destroy(gameObject);
     }
 
-    private void PlayHitNoise()
+    private void PlayHitNoise() //plays one shot of your impactSound SFX - no consideration for overlapping sounds in this case
     {
         audioSource.PlayOneShot(impactSound);//Play the audio you attach to the AudioSource component
     }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,71 +5,70 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    [SerializeField] Transform objectToPan;
-    [SerializeField] Transform targetToOrientTowards;
-    [SerializeField] GameObject weapon;
-    ParticleSystem weaponParticleEffect;
-    AudioSource weaponAudioSource;
-    EnemyMovement enemyMovement;
+    [SerializeField] Transform objectToPan; //Initialize the child object your are going to rotate to face your target
+    [SerializeField] Transform targetToOrientTowards; //Initialize the target towards whom you are going to orient 
+    [SerializeField] GameObject weapon; //Initialize the weapon game object that contains the particle system for firing bullets, impacting your target, etc (this is essentially your bullet firing particle system)
+    ParticleSystem weaponParticleEffect; //Initialize the component particle system of your weapon game object (this is the bullet firing particle system)
+    AudioSource weaponAudioSource; //Initialize the audiosource component of your weapon game object
+    EnemyMovement enemyMovement; //Initialize the EnemyMovement script variable, so you can extract variables/methods from it
 
-    [SerializeField] AudioClip firingSFX;
-    AudioSource audioSource;
+    [SerializeField] AudioClip firingSFX; //Initialize the bullet firing SFX 
 
-    bool isWeaponActive = false;
+    bool isWeaponActive = false; //Initialize the boolean that indicates whether or not your weapon game object is active (whether your bullets should be firing)
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyMovement = FindObjectOfType<EnemyMovement>();
-        audioSource = GetComponent<AudioSource>();
-        weaponParticleEffect = weapon.GetComponent<ParticleSystem>();
-        weaponAudioSource = weapon.GetComponent<AudioSource>();
+        enemyMovement = FindObjectOfType<EnemyMovement>(); //At start, find an object in our scene of type EnemyMovement (find the EnemyMovement script attached to your game object... 
+        // todo: this might need some refinemnent when multiple enemies show up... could cause a problme of finding multiple objects of this type
+        weaponParticleEffect = weapon.GetComponent<ParticleSystem>(); //At start, extract weapon game object's particle system component and set it to equal our initialized weaponParticleEffect variable
+        weaponAudioSource = weapon.GetComponent<AudioSource>(); //At start, extract weapon game object's audiosource component and set it to equal our initialized weaponAudioSource variable
     }
 
     // Update is called once per frame
     void Update()
     {
-        PanTowardEnemy();
-        FireAtEnemy();
+        PanTowardEnemy(); //orient your pivoting child game objects (the head of your tower and the bullet-firing particle system) to face the target location
+        FireAtEnemy(); // set your bullet-firing particle system to be active (fire your bullets) at the target when it is present in the scene 
     }
 
-    private void PanTowardEnemy()
+    private void PanTowardEnemy() //this method is responsible for pivoting child game objects (the head of your tower and the bullet-firing particle system) to face the target location
     {
-        objectToPan = transform.Find("Pivot");
-        if (enemyMovement != null)
+        objectToPan = transform.Find("Pivot"); //this is the game object parent of all your pivoting game objects
+        if (enemyMovement != null) //this tests whether an enemy (more specifically the EnemyMovement script attached to it) is present in the scene
         {
-            objectToPan.LookAt(targetToOrientTowards.position);
+            objectToPan.LookAt(targetToOrientTowards.position); //rotates your game object to the Vector3 position of your target
         }
     }
 
-    private void FireAtEnemy()
+    private void FireAtEnemy() //
     {
-        if (enemyMovement != null)
+        if (enemyMovement != null) //this tests whether an enemy (more specifically the EnemyMovement script attached to it) is present in the scene
         {
-            isWeaponActive = true;
-            PlayFiringSFXWhenEmitting();
+            isWeaponActive = true; //sets the status of your weapon-active status to true
+            PlayFiringSFXWhenEmitting(); //// this method sets your bullet-firing particle system to be active (fire your bullets) at the target when it is present in the scene AND when a particle is emitting
 
         }
         else
         {
-            isWeaponActive = false;
+            isWeaponActive = false; //sets the status of your weapon-active status to false
         }
         weapon.SetActive(isWeaponActive);
     }
 
-    private void PlayFiringSFXWhenEmitting()
+    private void PlayFiringSFXWhenEmitting() // this method sets your bullet-firing particle system to be active (fire your bullets) at the target when it is present in the scene 
     {
-        if (weaponParticleEffect.isEmitting)
+        if (weaponParticleEffect.isEmitting) //this lines tests whether a particle has been emitted from the particle system weaponParticleEffect of your weapon game object
         {
-            PlayFiringSFX();
+            PlayFiringSFX(); //plays a single clip of your firingSFX weapon-firing sound effect iff a clip is not already playing
         }
     }
 
-    private void PlayFiringSFX()
+    private void PlayFiringSFX() //plays a single clip of your firingSFX weapon-firing sound effect iff a clip is not already playing
     {
-        if (!weaponAudioSource.isPlaying)
+        if (!weaponAudioSource.isPlaying) //tests whether your not your clip is already playing
         {
-            weaponAudioSource.PlayOneShot(firingSFX);
+            weaponAudioSource.PlayOneShot(firingSFX); //plays a single clip of your firingSFX weapon-firing sound effect
         }
     }
 }
